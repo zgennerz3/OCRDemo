b49cf5afa276fe6a09e9c29df4d5aa4ccd0f39af
Juan <juan.rivaldo@connect.qut.edu.au>
2025-05-06 17:11:34 +1000

    Modified .gitignore to ignore the TessData folder

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,4 +9,6 @@ module com.example.ocrdemo.ocrdemo {
 
     opens com.example.ocrdemo.ocrdemo to javafx.fxml;
     exports com.example.ocrdemo.ocrdemo;
+    exports Controller;
+    opens Controller to javafx.fxml;
 }
