b84e7b07d89438382ba6da9fc38693c950de34db
z_gennerz3 <zanegenrich@gmail.com>
2025-05-06 15:59:20 +1000

    Added dependencies

--- src/main/java/com/example/ocrdemo/ocrdemo/MainController.java
+++ src/main/java/com/example/ocrdemo/ocrdemo/MainController.java
@@ -5,10 +5,5 @@ import javafx.scene.control.Label;
 
 public class MainController {
     @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private Label resultText;
 }
