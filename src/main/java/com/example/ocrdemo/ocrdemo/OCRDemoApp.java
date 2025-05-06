d3336a8f8371adfbebb485ccb57a61fa0880dab7
z_gennerz3 <zanegenrich@gmail.com>
2025-05-06 15:28:36 +1000

    Project setup cont.

--- src/main/java/com/example/ocrdemo/ocrdemo/OCRDemoApp.java
+++ src/main/java/com/example/ocrdemo/ocrdemo/OCRDemoApp.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class OCRDemoApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(OCRDemoApp.class.getResource("main-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1080, 720);
+        stage.setTitle("OCRDemo");
         stage.setScene(scene);
         stage.show();
     }
